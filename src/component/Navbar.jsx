3d9e53d9d9e801a37f89519f248d5c79b24b36ee
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-07-01 23:10:31 +0800

    finishing responsive navbar;

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { RiDrinksFill } from "react-icons/ri"
+import { RiDrinksFill, RiHexagonFill } from "react-icons/ri"
+import { LuSun, LuMoon } from "react-icons/lu"
+import { HiX, HiMenu} from "react-icons/hi"
 
 
 
@@ -16,13 +18,41 @@ const Navbar = () => {
   ]
 
   return (
-  <nav className='fixed top-0 left-0 bg-white/90 backdrop-blur-sm z-50 border-0 border-b-blue-600 shadow-sm w-full'>
-    <div className='dark bg-white h-10 w-full md:flex justify-center'>
+  <nav className='fixed top-0 left-0 bg-white/90 backdrop-blur-sm z-50 border-0 border-b-blue-600 shadow-sm w-full drop-shadow-md'>
+    <div className='bg-white container mx-auto py-2 px-4 w-full md:flex justify-center'>
         {/* company name */}
-        <div className='flex items-center flex-1/2'>
-        <RiDrinksFill/> <span className='text-emerald-500 mr-1 ml-1 font-medium'>Marga</span> Smoothies
+        <div className='container flex items-center justify-start sm:px-6 lg:px-8 h-15 md:h-18 sm:h-13'>
+        <RiDrinksFill className='mr-2 ml-2'/> <span className='text-emerald-500 font-medium mr-2'>Marga</span> Smoothies
+
+        {/* mobile nav */}
+        <button
+        className='md:hidden p-2 ml-40'
+        onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {isMenuOpen ? (<HiX className='h-6 w-6'/>) : (<HiMenu className='h-6 w-6'/>)}
+        </button>
+        {isMenuOpen && (
+            <div className='md:hidden mt-0 block bg-gray-100 border-t border-gray-100 py-4 w-full h-20'>
+              <div className='container mx-auto px-4 space-y-4'>
+                  {navLink.map((link, index) => (
+                    <a
+                    onClick={() => {
+                      setActiveLink(link.href);
+                      setIsMenuOpen(false);
+                    }}
+                    key={index}
+                    className={`block text-sm font-medium py-2
+                      ${activeLink === link.href ? 'text-blue-600' : 'text-gray-500 hover:text-gray-800'}`}
+                      href={link.href}>{link.label}</a>
+                  ))}
+              </div>
+            </div>
+        )}
         </div>
-      <div className='hidden md:flex items-center gap-10'>
+        
+
+
+        {/* desktop menu */}
+      <div className='hidden md:flex items-center justify-between gap-10'>
         {
           navLink.map((link, index) => (
             <a key={index} href={link.href}
@@ -34,8 +64,11 @@ const Navbar = () => {
           ))
         }
       </div>
+      
+        
 
     </div>
+     
   </nav>
   )
 }
